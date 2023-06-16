b3563a2b03ccde1fbed31d91bd6bff725666625e
rogue <r.o.g.u.e@outlook.com>
2023-06-16 05:17:15 -0700

    c 0x01

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'print 0-9 separated by commas'
+ * Return : always 0
+*/
+int main(void)
+{
+	int i = '0';
+	while (i <= '9')
+	{
+		putchar(i);
+		if(i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
