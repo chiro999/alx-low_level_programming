fb478ff8237b4724c9bc276b38481843b54dd390
rogue <r.o.g.u.e@outlook.com>
2023-06-16 05:05:32 -0700

    c 0x01

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is 0", n, last_digit);
 	}
-	else if (last_digit < 6 && last_digit != 0)
+	else
 	{
 		printf("Last digit of %d is %d and less than 6 and not 0", n, last_digit);
 	}
