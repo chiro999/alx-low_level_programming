85183022468cadc13b7516a7d0439a17b9a50106
rogue <r.o.g.u.e@outlook.com>
2023-06-20 00:45:07 -0700

    0x02- 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include "unistd.h"
+/**
+ * main - Entry point
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	write(2, "_putchar\n", 9);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - Make the alphabet
+ * Return: void
+*/
+void print_alphabet(void)
+{
+	char x ;
+	for (x = 97; x <= 122; x++)
+	{
+	_putchar(x);
+	}
+	_putchar('\n');
+}
