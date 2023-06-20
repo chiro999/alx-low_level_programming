aa672ca40fbef1ff006955ae4beb8c0eb772c3e4
rogue <r.o.g.u.e@outlook.com>
2023-06-20 11:18:17 -0700

    5 updated

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_sign - Prints the sign of a number
- * @n: The number to be checked
+ * @x: The number to be checked
  * Return: 1 for positive num, -1 for negative num else zero
 */
 
