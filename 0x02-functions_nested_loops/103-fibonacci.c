c878148d78dbb7f052c8e0a2747addb208c6d222
rogue <r.o.g.u.e@outlook.com>
2023-06-20 14:52:26 -0700

    15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Prints the sum of even fibonacci numbers less than 4 million
+ * Return: Always 0
+*/
+int main(void)
+{
+	 int x = 0;
+	 long y = 1, z = 2, total = z;
+
+	while (z + y < 4000000)
+	{
+	y += z ;
+	if (y % 2 == 0)
+	total +=z;
+	y = z - y;
+	++x;
+	}
+        printf("%ld\n", total);
+	return (0);	
+}
