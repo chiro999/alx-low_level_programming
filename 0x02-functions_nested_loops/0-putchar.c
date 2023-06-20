35364a45e4e728c940cdd77238e460a766fb69b7
rogue <r.o.g.u.e@outlook.com>
2023-06-20 06:56:10 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,6 +7,13 @@
 
 int main(void)
 {
-	write(2, "_putchar\n", 9);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');;
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r'');
 	return (0);
 }
