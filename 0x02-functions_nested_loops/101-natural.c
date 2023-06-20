091675310ee12aa787a8d7b70cd083f63df40620
rogue <r.o.g.u.e@outlook.com>
2023-06-20 11:35:35 -0700

    11

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,6 +6,7 @@
 int main(void)
 {
 	int i, z = 0;
+
 	while (i < 1024)
 	{
 	if ((i % 3 == 0) || (i % 5 == 0))
@@ -14,6 +15,6 @@ int main(void)
 	}
 	i++;
 	}
-	printf("%d\n",z);
+	printf("%d\n", z);
 	return (0);
 }
