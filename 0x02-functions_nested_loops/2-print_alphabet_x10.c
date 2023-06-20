3e3e4abc96c4c7e3dd52d658afd531cb6ea49d92
rogue <r.o.g.u.e@outlook.com>
2023-06-20 01:08:42 -0700

    3 complete

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Make alphabet x10 times
+ * Return: void
+*/
+
+void print_alphabet_x10(void)
+{
+	char x;
+	int i=0;
+	
+	while (i < 10)
+	{
+		for (x = 97; x <= 122; x++)
+		{
+			_putchar(x);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islower - Checks for lowercase character
+ * @c: The character to be checked
+ * Return: 1 for lowercase character or 0 for anything else
+*/
+
+int _islower(int x)
+{
+	if (x >= 'a' && x <= 'z')
+	{
+		return (1);
+	}
+		return (0);
+}
+ 
