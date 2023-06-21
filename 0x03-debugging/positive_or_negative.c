7a7e24d8da2b848a0f8a65da387d2074ddba6237
rogue <r.o.g.u.e@outlook.com>
2023-06-21 08:50:22 -0700

    200

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,11 @@
+#include <main.h>
+
+void positive_or_negative(int x)
+{
+	if (i > 0)
+		printf("%d is positive\n", x);
+	else if (i < 0)
+		printf("%d is negative\n", x);
+	else
+		printf("%d is zero\n", x);
+}
