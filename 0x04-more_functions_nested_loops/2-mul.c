574ae017a9650a5fcc314265ffb4c7898b48cb5d
rogue <r.o.g.u.e@outlook.com>
2023-06-22 04:50:26 -0700

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - Prints the result of two numbers multiplied together
+ * @x - First number to be operated on
+ * @y - Second number to be operated on
+ * Return: The result of the xy multiplication
+*/
+
+int mul(int x, int y)
+{
+	return (x * y);
+}
