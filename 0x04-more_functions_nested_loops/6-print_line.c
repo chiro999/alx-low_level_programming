3358dc73c96f7f06fd0d17910d87754db44a5f8b
rogue <r.o.g.u.e@outlook.com>
2023-06-22 05:23:54 -0700

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_line - Draws a straight line according to value n given
+ * @n: Number of lines to draw
+ * Return: Nothing!
+*/
+
+void print_line(int n)
+{
+	int x;
+
+	if (n <=0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (x = 0; x < n; x++)
+	{
+	_putchar('_');
+	}
+	_putchar('\n');
+	}
+}
