88a865d1f75b02c44b98dad91a5411ae7a7e430f
rogue <r.o.g.u.e@outlook.com>
2023-06-26 01:57:42 -0700

    3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to be printed
+ */
+void _puts(char *str)
+{
+	int x;
+
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		_putchar(str[x:wq]);
+	}
+	_putchar('\n');
+}
