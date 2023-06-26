fa5899092a78152dbf72be92a81ab66b668ba5ef
rogue <r.o.g.u.e@outlook.com>
2023-06-26 02:07:46 -0700

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: string to be printed
+ */
+void print_rev(char *s)
+{
+	int i, r, str_len;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	str_len = i;
+	r = str_len -1;
+
+	for (; r >= 0; r--)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+}
