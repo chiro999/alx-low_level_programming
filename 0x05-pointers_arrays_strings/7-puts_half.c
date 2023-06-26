9bebef26ceff706f2a8773f8a36c95420cedf5ba
rogue <r.o.g.u.e@outlook.com>
2023-06-26 06:47:19 -0700

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *puts_half - prints half of a string 
- *followed by a new line
- *@str: string to be printed
-*/
+ * puts_half - Prints half of a string
+ * @str: The string to print
+ * Return: void
+ */
 
 void puts_half(char *str)
 {
