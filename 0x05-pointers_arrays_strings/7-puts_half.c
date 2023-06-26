339c8734bdb3fe65567e910d273bb54cfdf338d9
rogue <r.o.g.u.e@outlook.com>
2023-06-26 03:43:48 -0700

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string 
+ * followed by a new line
+ * @str: string to be printed
+*/
+
+void puts_half(char *str)
+{
+	int i = 0;
+	int x;
+
+	while (str[i] != '\0')
+	{
+	x++;
+	}
+	if (!(i % 2 == 0)){
+	x = (i - 1) / 2 ;
+	x += 1;
+	}
+	else
+	{
+	x = i/2;
+	}
+
+	for (; x < i; x++)
+	{
+	_putchar(str[i]);
+	}
+	_putchar('\n');
+}
