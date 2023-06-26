bbb7d5e80fd260dedae9707e8cd4a75fd90e5d19
rogue <r.o.g.u.e@outlook.com>
2023-06-26 01:44:12 -0700

    0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - takes a pointer to an int and updates its value to 98
+ * @n: The input integer pinter
+ * Retun: nothing
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
