1480e14bfac4fd0d9c47f0374fd8b0389a1e3b78
rogue <r.o.g.u.e@outlook.com>
2023-07-13 10:03:52 -0700

    3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,7 +16,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	arr = malloc((sizeof(int) * range) + 1);
+	arr = malloc((sizeof(int) * range) + 4);
 
 	if (arr == NULL)
 		return (NULL);
