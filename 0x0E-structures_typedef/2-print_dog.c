2480648bc62a7ae492327953ccad6c0815c2ac00
rogue <r.o.g.u.e@outlook.com>
2023-07-17 17:23:11 -0700

    2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,7 +12,7 @@ void print_dog(struct dog *d)
 	if (!d)
 		return;
 
-	printf("Name: %s\n", d->name ? d->name : "(nil)");
+	printf("Name: %s\n", !(d->name) ? "(nil)" : d->name);
 	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 }
