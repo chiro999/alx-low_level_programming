c019a096bfd622217ddb1952411a31245473b87c
rogue <r.o.g.u.e@outlook.com>
2023-07-19 00:53:35 -0700

    1

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,4 @@
-#inlcude "function_pointers.h"
+#include "function_pointers.h"
 
 /**
  * print_name - Prints a name
---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function given as a parameter on each element of an array.
+ * @array: array of elements
+ * @size: size of the array
+ * @action: function pointer
+ *
+ * Return: elements of the array after passing through the action function
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i = 0;
+
+	if (array && action && size > 0)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i];
+	}
+}
