1e6b0d41ffdc55f17a57c19a6fc0ddcef33cf24b
rogue <r.o.g.u.e@outlook.com>
2023-07-19 16:43:36 -0700

    100

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,8 +10,8 @@
  */
 int main(int argc, char *argv[])
 {
-	int bytes, i;
-	char *arr;
+	int byte_counter, i, last_byte;
+	char *op_arr;
 
 	if (argc != 2)
 	{
@@ -19,24 +19,26 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
-	bytes = atoi(argv[1]);
+	byte_counter = atoi(argv[1]);
 
-	if (bytes < 0)
+	if (byte_counter < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
 
-	arr = (char *)main;
+	op_arr = (char *)main;
 
-	for (i = 0; i < bytes; i++)
+	last_byte = byte_counter - 1;
+
+	for (i = 0; i < byte_counter; i++)
 	{
-		if (i == bytes - 1)
+		if (i == last_byte)
 		{
-			printf("%02hhx\n", arr[i]);
+			printf("%02hhx\n", op_arr[i]);
 			break;
 		}
-		printf("%02hhx ", arr[i]);
+		printf("%02hhx ", op_arr[i]);
 	}
 	return (0);
 }
