39588672fbe3883d94e5d623bb73799f7c5f0820
rogue <r.o.g.u.e@outlook.com>
2023-07-20 16:24:40 -0700

    3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,39 +4,42 @@
 #include "variadic_functions.h"
 
 /**
- * print_all - prints anything.
- * @format: types of arguments passed to the function.
+ * print_all - prints parameters of any type
+ * @format: types of parameters to be operated on by the function
  * Return: nothing.
  */
 void print_all(const char * const format, ...)
 {
-	va_list arguments;
-	unsigned int i = 0;
+	va_list parameters;
 	char *string;
+	unsigned int i = 0;
 
-	va_start(arguments, format);
-	while (format && format[i])
+	va_start(parameters, format);
+	while (format && format[i] != '\0')
 	{
 		switch (format[i++])
 		{
-			case 'c':
-				printf("%c", va_arg(arguments, int));
-				break;
 			case 'i':
-				printf("%d", va_arg(arguments, int));
+				printf("%d", va_arg(parameters, int));
+				break;
+			case 'c':
+				printf("%c", va_arg(parameters, int));
 				break;
 			case 'f':
-				printf("%f", va_arg(arguments, double));
+				printf("%f", va_arg(parameters, double));
 				break;
 			case 's':
-				string = va_arg(arguments, char *);
-				printf("%s", string  ? string : "(nil)");
+				string = va_arg(parameters, char *);
+				printf("%s", !string  ? "(nil)" : string);
 				break;
 			default:
 				continue;
 		}
-		if (format[i])
+		if (format[i] != '\0')
 			printf(", ");
+		
+
 	}
 	printf("\n");
+	va_end(parameters);
 }
