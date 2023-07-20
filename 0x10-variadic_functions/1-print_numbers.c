0f4f17812218457765becac42afc3cf8030944ef
rogue <r.o.g.u.e@outlook.com>
2023-07-20 08:11:46 -0700

    1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,7 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(numbers, int));
 		
-	     if ((i < n - 1) && separator)
+	 if ((i < n - 1) && separator)
 		printf("%s", separator);
 
 	     i++;
