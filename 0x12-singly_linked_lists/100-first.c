ddbba72534c85e9fc94ebfd5484645d13f14fe06
chiro999 <r.o.g.u.e@outlook.com>
2023-07-27 06:01:13 -0700

    100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * bmain - function executed before main
+ * pre - function executed before main
  * Return: no return.
  */
 
-void __attribute__ ((constructor)) bmain()
+void __attribute__ ((constructor)) pre()
 {
 		printf("You're beat! and yet, you must allow,\n");
 		printf("I bore my house upon my back!\n");
