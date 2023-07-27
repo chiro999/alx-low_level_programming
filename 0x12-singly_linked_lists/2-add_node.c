2fc0455227dbcd2aaa4ca4816dceacb5bf461933
chiro999 <r.o.g.u.e@outlook.com>
2023-07-27 05:28:42 -0700

    update1

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -17,7 +17,7 @@ list_t *add_node(list_t **head, const char *str)
 	while (str[i] != '\0')
 		i++;
 
-	new = malloc(sizeof(list_t));
+	node_nueve = malloc(sizeof(list_t));
 	if (node_nueve == NULL)
 		return (NULL);
 
