834ed70a3e9dcfceb319e172c6b5efcdc07ff140
chiro999 <r.o.g.u.e@outlook.com>
2023-07-27 08:07:48 -0700

    101

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,7 +1,9 @@
-          global    main
+/* in x86-64 8 more registers with prefixes "r" were added */         
+	  global    main
           extern    printf
 main:
 	  mov   rdi, string
+/* xor a register with itself to set it to 0 */
 	  xor   rax, rax
 	  call  printf
 	  mov 	rax, 0
