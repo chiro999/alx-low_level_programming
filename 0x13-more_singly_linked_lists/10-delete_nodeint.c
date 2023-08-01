d81aae27d156b6d602981efa8d4027b269e0504e
chiro999 <r.o.g.u.e@outlook.com>
2023-08-01 01:09:04 -0700

    10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -24,7 +24,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 
-	while (i < index - 1)
+	while (i < penultimate)
 	{
 		if (temp = NULL || (temp->next == NULL))
 			return (-1);
