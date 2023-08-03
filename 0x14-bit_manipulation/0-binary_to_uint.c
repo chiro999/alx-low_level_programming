7d6f8da6a6504b254b32d669f6ee17ce6fc2f41f
chiro999 <r.o.g.u.e@outlook.com>
2023-08-03 05:42:54 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,26 +1,36 @@
 #include <stdio.h>
-#include "main.h"
+#include <man.h>
 
 /**
  * binary_to_uint - convert a binary number to an unsigned int
  * @b: char string
- * Return: converted decimal number or 0 if there is an unconvertable char
+ * Return: converted decimal number or 0 if there is an unconvertible char
  */
-
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int dec = 0;
- 	int index = 0;
+	int index = 0;
 
 	if (b == NULL)
 		return 0;
+   
+    
+        char selected_char = b[index];
 
-	/* while loop to check if the number b is a binary number */
-	while (b[index] == '0' || b[index] == '1')
- 	 {  
-    		dec <<= 1;
-    		dec += b[index]-'0';
-   	 	index++;
- 	 }
-  return dec;
+        // Check if the character is '0' or '1'
+        if (current_char == '0' || current_char == '1')
+        {
+            // Convert the character to integer and update the decimal value
+            dec = dec * 2 + (current_char - '0');
+        }
+        else
+        {
+            // If the character is not '0' or '1', return 0 (invalid input)
+            return 0;
+        }
+
+        index++; // Move to the next character
+    }
+
+    return dec;
 }
