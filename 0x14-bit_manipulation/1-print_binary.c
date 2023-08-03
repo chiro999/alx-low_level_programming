4d3aac2a087f3d861a7cbc395db881f30dd314dd
chiro999 <r.o.g.u.e@outlook.com>
2023-08-03 05:08:53 -0700

    0

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,7 +7,7 @@
 void print_binary(unsigned long int n)
 {
 	int i = 63;
-       	int count = 0;
+       	int ones = 0;
 	unsigned long int shifted;
 	
 	while (i >= 0)
@@ -18,14 +18,14 @@ void print_binary(unsigned long int n)
 		if (shifted & 1)
 		{
 			_putchar('1');
-			count++;
+			ones++;
 		}
-		else if (count)
+		else if (ones)
 			_putchar('0');
 
 		i--;
 	}
-	if (count == NULL)
+	if (!ones)
 		_putchar('0');
 }
 
