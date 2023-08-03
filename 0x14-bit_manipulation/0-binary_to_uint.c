68d2a319782257f2051b3ab1985c56258361598e
chiro999 <r.o.g.u.e@outlook.com>
2023-08-03 06:05:02 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,18 +13,21 @@ unsigned int binary_to_uint(const char *b)
  	int index = 0;
 
 	if (b == NULL)
-		return 0;
+		return (0);
 	
-	if (b[index] < '0' || b[index] > '1')
-		return 0;
+	
 	/* while loop to check if the number b is a binary number */
-	while (b[index] == '0' || b[index] == '1')
+	while (b[index])
  	 { 
-
-			 
+		
+	        if (b[index] < '0' || b[index] > '1')
+			return 0;
+		if(b[index] == '0' || b[index] == '1')
+       		{			
     		dec <<= 1; /* this is the same as the dec * 2 */
     		dec = dec + b[index]-'0'; /* this is the same as (2 * dec) + (b[index] - '0') */
    	 	index++;
+		}
  	 }
   return dec;
 }
