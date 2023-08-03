240969d97c260d7a52ece58228905785eb738437
chiro999 <r.o.g.u.e@outlook.com>
2023-08-03 07:41:33 -0700

    100

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	char *c;
+
+	
+	endian_or_not = (char *) & 1
+
+	return ((int)* endian_or_not);
+}
