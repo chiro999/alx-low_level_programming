e38006a7fc025a1e11d828bb5d6e8a85d55148ca
chiro999 <r.o.g.u.e@outlook.com>
2023-08-07 03:23:58 -0700

    v2

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -73,7 +73,7 @@ int main(int argc, char *argv[])
 	file_des = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
-		if (from == -1 || r == -1)
+		if (file_src == -1 || file_r == -1)
 		{
 			dprintf(STDERR_FILENO,
 				"Error: Can't read from file %s\n", argv[1]);
@@ -81,8 +81,8 @@ int main(int argc, char *argv[])
 			exit(98);
 		}
 
-		w = write(to, buffer, r);
-		if (to == -1 || w == -1)
+		file_w = write(file_des, buffer, file_r);
+		if (file_src == -1 || file_w == -1)
 		{
 			dprintf(STDERR_FILENO,
 				"Error: Can't write to %s\n", argv[2]);
@@ -90,10 +90,10 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 
-		r = read(from, buffer, 1024);
-		to = open(argv[2], O_WRONLY | O_APPEND);
+		file_r = read(file_src, buffer, 1024);
+		file_des = open(argv[2], O_WRONLY | O_APPEND);
 
-	} while (r > 0);
+	} while (file_r > 0);
 
 	free(buffer);
 	close_file(file_src);
