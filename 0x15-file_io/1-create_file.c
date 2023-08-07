8bb9cb2bb21cbc5c10747d5ac352d39b9de4e1fd
chiro999 <r.o.g.u.e@outlook.com>
2023-08-07 00:52:12 -0700

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (i = 0; text_content[i]; i++;
+		for (i = 0; text_content[i]; i++);
 	}
 
 	file_o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
